0a33aada193a1e1545f722154deaee76966b8ef3
Joey Gouly <joey.gouly@gmail.com>
2025-12-11 09:10:56 +0000

    exit on error

--- stub.cpp
+++ stub.cpp
@@ -2,10 +2,12 @@
 
 void uzenet(const char* text1, const char* text2, const char* text3) {
     printf("%s %s %s\n", text1, text2, text3);
+    mv_exit();
 }
 
 void hiba(const char* text1, const char* text2, const char* text3) {
     printf("%s %s %s\n", text1, text2, text3);
+    mv_exit();
 }
 
 int s_random(int maximum) { return 0; }
