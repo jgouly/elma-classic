d0e724c9afe7fc947b7380dbbc772ca5c1e5769e
Joey Gouly <joey.gouly@gmail.com>
2025-12-17 18:57:23 +0000

    WIP: shirt

--- affine_pic.cpp
+++ affine_pic.cpp
@@ -34,6 +34,56 @@ affine_pic::affine_pic(const char* filename, pic8* pic) {
     delete pic;
 }
 
+affine_pic::affine_pic(int w, int h) {
+    width = w;
+    height = h;
+    if (width > 255 || height > 255) {
+        internal_error("affine_pic size > 255!");
+    }
+    int length = height * 256;
+    pixels = new unsigned char[length];
+    if (!pixels) {
+        external_error("affine_pic out of memory!");
+    }
+}
+
+#define BMP_HEADER_SIZE 1078
+#define BMP_WIDTH_EOLMAX 256
+#define BMP_WIDTH 149
+#define BMP_WIDTH_PADDED 152
+#define BMP_EOL_ARRAY_SIZE (BMP_WIDTH_EOLMAX * BMP_HEIGHT)
+#define BMP_HEIGHT 101
+#define BMP_PIXEL_ARRAY_SIZE (BMP_WIDTH_PADDED * BMP_HEIGHT)
+
+affine_pic* affine_pic::from_bmp(const char* filename) {
+    unsigned char data[BMP_PIXEL_ARRAY_SIZE] = {0};
+    FILE* f = fopen(filename, "rb");
+    if (!f) {
+        return nullptr;
+    }
+
+    fseek(f, BMP_HEADER_SIZE, SEEK_SET);
+    fread(data, BMP_PIXEL_ARRAY_SIZE, 1, f);
+    affine_pic* k = new affine_pic(149, 101);
+
+    /*
+        k->lyuk = 0;
+        for (int y = 0; y < k->ysize; y++) {
+            for (int x = 0; x < k->xsize; x++) {
+                k->tomb[y * 256 + x] = x % 256;
+            }
+        }*/
+    k->transparency = data[0];
+    for (int i = 0; i < BMP_EOL_ARRAY_SIZE; i++) {
+        if ((i % BMP_WIDTH_EOLMAX < BMP_WIDTH) && (i / BMP_WIDTH_EOLMAX < BMP_HEIGHT)) {
+            k->pixels[i] = data[BMP_WIDTH_PADDED * ((BMP_HEIGHT - 1) - i / BMP_WIDTH_EOLMAX) +
+                                (i % BMP_WIDTH_EOLMAX)];
+        }
+    }
+
+    return k;
+}
+
 affine_pic::~affine_pic() {
     if (pixels) {
         delete pixels;
