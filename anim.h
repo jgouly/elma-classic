8329bff154af2a2d3feb058187716155aedd92af
Joey Gouly <joey.gouly@gmail.com>
2025-12-18 00:30:32 +0000

    feat(zoom): add scale parameter to anim() constructor

--- anim.h
+++ anim.h
@@ -11,7 +11,7 @@ class anim {
     int frame_count;
     pic8* frames[ANIM_MAX_FRAMES];
 
-    anim(pic8* source_sheet, const char* error_filename);
+    anim(pic8* source_sheet, const char* error_filename, double scale = 1.0);
     ~anim();
     pic8* get_frame_by_time(double time);
     pic8* get_frame_by_index(int index);
